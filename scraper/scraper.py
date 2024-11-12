3b956c80c9ea9c184205939da0946fc599bff44a
Carina Payleman <rociocarina2@gmail.com>
2024-11-12 17:36:53 -0300

    refactor: rename and enhance scraper to scraper_xml

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,17 +1,21 @@
 from playwright.sync_api import sync_playwright
 
 
-def scrape():
+def scrape_xml():
     url = "https://clerk.house.gov/evs/2024/roll455.xml"
     
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
+        
+        def handle_response(response):
+            if response.url == url:
+                print(response.text())
+
+        page.on("response", handle_response)
         page.goto(url)
-        content = page.content()
-        print(content)
         browser.close()
 
 
 if __name__ == "__main__":
-    scrape()
+    scrape_xml()
