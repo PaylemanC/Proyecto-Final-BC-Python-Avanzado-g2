67f19465609122aef2033f3d6b5c05318765f55c
Carina Payleman <rociocarina2@gmail.com>
2024-11-20 23:46:43 -0300

    feat(templates): add base setup and initial test for Django templates

--- rollcall_votes/rollcall_votes/urls.py
+++ rollcall_votes/rollcall_votes/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('rollcall/', views.rollcall, name='rollcall'),
+    path('', views.dashboard, name='dashboard')
 ]
