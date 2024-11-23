6941bc5fc5a5fc43621c566a72fca8896dccf811
Elias <evelazquez2604@gmail.com>
2024-11-23 17:30:00 -0300

    feat: Introduce new environment variable
    This commit introduces the new env variable CONGRESS_API_KEY, needed for the upcoming module that will get congress data, like members, bills, etc from the congress api.
    
    The "requests" library is added as well to the requirements.txt file and more file extensions are added to the .gitignore file.

--- config.py
+++ config.py
@@ -6,9 +6,10 @@ if os.path.exists('.env'):
     
 ENVIRONMENT :str = os.getenv('ENVIRONMENT', 'PROD')
 HR_VOTES_YEAR :str | None = os.getenv('HR_VOTES_YEAR', 2024)
+CONGRESS_API_KEY :str = os.getenv('CONGRESS_API_KEY')
 CONGRESS :str = os.getenv('CONGRESS', 118)
 CONGRESS_SESSION :str = os.getenv('CONGRESS_SESSION', 2)
 
 
 if __name__ == "__main__":
-    print(ENVIRONMENT)
+    print(f"ENVIRONMENT: {ENVIRONMENT}")
