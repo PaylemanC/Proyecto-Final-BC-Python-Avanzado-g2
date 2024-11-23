f6f5c287b075d9ce6776592edd7dbe3e710ddc6d
Carina Payleman <rociocarina2@gmail.com>
2024-11-22 23:35:02 -0300

    refactor: restructure essential HTML files, and create sidebar component with basic routes

--- rollcall_votes/dashboard/urls.py
+++ rollcall_votes/dashboard/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),  
+    path('rollcall_list/', views.rollcall_list, name="rollcall_list"),
+    path('legislators', views.member_list, name="member_list"),
     path('rollcall/', views.rollcall, name='rollcall'),  
 ]
--- rollcall_votes/data/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('', views.parties_list, name='parties_list'),
-]
