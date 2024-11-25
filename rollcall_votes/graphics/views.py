346f2344a71eb03a2c1678e757a90640de3507f7
Carina Payleman <rociocarina2@gmail.com>
2024-11-25 15:37:06 -0300

    style(graphics): Update line color and add gradient background to law conversion chart

--- rollcall_votes/graphics/views.py
+++ rollcall_votes/graphics/views.py
@@ -23,24 +23,34 @@ def generate_project_type_chart(data):
 
 def generate_law_conversion_line_chart(data):
     df = pd.DataFrame(data)
-    
-    line = alt.Chart(df).mark_line(point=True).encode(
+    line = alt.Chart(df).mark_line(point=alt.OverlayMarkDef(filled=False, fill="white")).encode(
         x=alt.X('action_date:T', title='Date'),
         y=alt.Y('total:Q', title='Number of Laws Passed'),
+        strokeWidth=alt.value(2.5),
+        color=alt.value('#F8BD7A'),
         tooltip=[
             alt.Tooltip('action_date:T', title='Date'),
             alt.Tooltip('total:Q', title='Laws Passed')
         ]
     )
 
-    area = alt.Chart(df).mark_area(opacity=0.0).encode(
-        x='action_date:T',
-        y='total:Q',
-        tooltip=[
-            alt.Tooltip('action_date:T', title='Date'),
-            alt.Tooltip('total:Q', title='Laws Passed')
-        ]
-    )
+    area = alt.Chart(df).mark_area(
+            line={'color': '#F8BD7A'}, 
+            color=alt.Gradient(
+                gradient='linear',
+                stops=[
+                    alt.GradientStop(color='white', offset=0),
+                    alt.GradientStop(color='#F8BD7A', offset=1)
+                ],
+                x1=1, x2=1, y1=1, y2=0 )
+            ).encode(
+                x='action_date:T',
+                y='total:Q',
+                tooltip=[
+                    alt.Tooltip('action_date:T', title='Date'),
+                    alt.Tooltip('total:Q', title='Laws Passed')
+                ]
+            )
 
     law_line_chart = (area + line).properties(
         width=600,
