5219e071d4f9a31a0dfd46feaa184d22a00b3964
Carina Payleman <rociocarina2@gmail.com>
2024-11-24 22:10:05 -0300

    Remove all rollcalls-related templates, views and urls. Also, move legislator logic to member_list template/view

--- rollcall_votes/dashboard/urls.py
+++ rollcall_votes/dashboard/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),  
-    path('rollcall_list/', views.rollcall_list, name="rollcall_list"),
-    path('legislators', views.member_list, name="member_list"),
-    path('rollcall/', views.rollcall, name='rollcall'),  
+    path('', views.dashboard, name='dashboard'), 
+    path('legislators', views.member_list, name="member_list")
 ]
--- rollcall_votes/dashboard/views.py
+++ rollcall_votes/dashboard/views.py
@@ -5,23 +5,17 @@ from data.models import Members
 
 def dashboard(request):
     chart_html = generate_chart()
-    members = Members.objects.all()
-    members_count = Members.objects.count()
     
     return render(request, 'dashboard.html', {
-        'chart_html': chart_html,
-        'members': members,
-        'members_count': members_count
+        'chart_html': chart_html
         })  
 
 
-def rollcall_list(request):
-    return render(request, 'rollcall_list.html')
-
-
-def rollcall(request):
-    return render(request, 'rollcall.html')  
-
-
 def member_list(request):
-    return render(request, 'member_list.html')
+    members = Members.objects.all()
+    members_count = Members.objects.count()
+    
+    return render(request, 'member_list.html', {
+        'members': members,
+        'members_count': members_count
+    })
