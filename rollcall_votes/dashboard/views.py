8c00384a1d1ec69f0480ee0c2bc349a6b543f9a2
Carina Payleman <rociocarina2@gmail.com>
2024-11-25 08:09:46 -0300

    feat: Add calculated data to dashboard page
    Visualizations and summaries:
    - members_count: Total number of members.
    - bills_by_type: Total number of bills by type, donut chart with Vega-Altair.
    - laws_count: Total bills that became laws.
    - last_bills: Table with 10 last bills updated based on action_date

--- rollcall_votes/dashboard/views.py
+++ rollcall_votes/dashboard/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from graphics.views import generate_chart
-from data.models import Members
+# from graphics.views import generate_chart
+from data.models import Members, Bills
+from django.db.models import Count
+from graphics.views import generate_project_type_chart
 
 
 def dashboard(request):
-    chart_html = generate_chart()
+    members_count = Members.objects.count()
+    bills_by_type = Bills.objects.values('type').annotate(total=Count('type')) # [{'type': 'S', 'total': 50}, {'type': 'HR', 'total': 60}, {'type': 'SRES', 'total': 90}]
+    laws_count = Bills.objects.filter(action_text__startswith="Became Public Law No:").count()    
+    bill_type_chart = generate_project_type_chart(bills_by_type)
+    last_bills = Bills.objects.all().order_by('-action_date')[:10]
     
     return render(request, 'dashboard.html', {
-        'chart_html': chart_html
+        'members_count': members_count,
+        'bills_by_type': bills_by_type,
+        'laws_count': laws_count,
+        'bill_type_chart': bill_type_chart,
+        'last_bills': last_bills
         })  
 
 
--- rollcall_votes/graphics/views.py
+++ rollcall_votes/graphics/views.py
@@ -2,16 +2,21 @@ import altair as alt
 import pandas as pd
 
 
-def generate_chart():
-    data = pd.DataFrame({
-        'category': ['A', 'B', 'C', 'D', 'E'],
-        'value': [30, 20, 50, 40, 60]
-    })
+def generate_project_type_chart(data):
+    df = pd.DataFrame(data)
+
+    df['percentage'] = (df['total'] / df['total'].sum()) * 100
     
-    chart = alt.Chart(data).mark_bar().encode(
-        x='category',
-        y='value',
-        tooltip=['category', 'value'] 
+    df['percentage'] = df['percentage'].round(2)
+    
+    df = df.sort_values('percentage')
+    
+    chart = alt.Chart(df).mark_arc(innerRadius=50).encode(
+        theta='percentage:Q',
+        color='type:N',
+        tooltip=['type:N', 'percentage:Q'],
+        text='type:N',
+        order=alt.Order('percentage:Q', sort='ascending')
     )
     
     return chart.to_html()
