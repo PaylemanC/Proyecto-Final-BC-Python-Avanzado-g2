896af258a3b4fada740321983d8ff6e8a3329115
Carina Payleman <rociocarina2@gmail.com>
2024-11-25 18:29:49 -0300

    chore(cleanup): Remove unused default Django apps from installed apps: - admin - auth - contenttypes - sessions - also, deleted admin.py files.
    This change prevents unnecesary migrations and simplifies project's configuration.

--- rollcall_votes/rollcall_votes/settings.py
+++ rollcall_votes/rollcall_votes/settings.py
@@ -29,32 +29,18 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-DJANGO_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
+INSTALLED_APPS = [
     'django.contrib.messages',
-    'django.contrib.staticfiles'
-]
-
-THIRD_APPS = []
-
-LOCAL_APPS = [
+    'django.contrib.staticfiles',
     'data',
     'dashboard',
     'graphics'
 ]
 
-INSTALLED_APPS = THIRD_APPS + DJANGO_APPS  + LOCAL_APPS
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -69,7 +55,6 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -95,20 +80,20 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
 
 
 # Internationalization
